a7294812fd32f938c963aa2d023677451e766f67
Aman Yadav <ay2728850@gmail.com>
2025-12-07 17:06:23 +0530

    Trying and failed to Use tool calling

--- src/llm_engine.py
+++ src/llm_engine.py
@@ -1,9 +1,22 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.messages import AIMessage
+from langchain.tools import BaseTool, tool
 from langchain_core.prompts import ChatPromptTemplate
 from dotenv import load_dotenv
+from utils.tools.music_tool import SpotifyTool
 import os
 
 load_dotenv()
+
+@tool
+def play_music(query: str) -> str:
+    """This tools takes query as the song name by author and plays it on spotify"""
+    return SpotifyTool().play_music(query)
+
+@tool
+def stop_music() -> str:
+    """This tool stops the music playing on spotify"""
+    return SpotifyTool().stop_music()
 
 class LLMEngine:
     """
@@ -20,6 +33,8 @@ class LLMEngine:
             google_api_key=os.getenv("GEMINI_API_KEY"),
             temperature=0.7 # A little creativity, but let's not get too crazy.
         )
+
+        self.llm = self.llm.bind_tools([play_music, stop_music])
 
         # The "Personality" of the AI.
         # We tell it to be a helpful voice assistant, not a novelist.
@@ -42,9 +57,41 @@ class LLMEngine:
         """
         print("🧠 Thinking (with LangChain)...")
         # Stream the response so we can pipeline the audio generation.
-        return self.chain.stream({"text": text})
+        stream = self.chain.stream({"text": text})
+        
+        for chunk in stream:
+            if chunk.type == "ai":
+                if chunk.content:
+                    yield chunk.content if hasattr(chunk, "content") else chunk
+            
+
+                if hasattr(chunk, "tool_calls") and chunk.tool_calls:
+                    for tool_call in chunk.tool_calls:
+                        tool_name = tool_call.name
+                        tool_args = tool_call.args
+                        
+                        print(f"\n LLM requested tool: {tool_name}({tool_args})")
+
+                        tool_obj: BaseTool = self.get_tool(tool_name)
+                        tool_result = tool_obj.invoke(tool_args)
+
+                        print(f"Tool result: {tool_result}")
+
+                        follow_up = self.chain.invoke({"text": tool_result})
+
+                        yield follow_up.content
+            else:
+                yield chunk.content if hasattr(chunk, "content") else chunk
+
+
+
+    def get_tool(self, name:str):
+        for tool in [play_music, stop_music]:
+            if tool.name == name:
+                return tool
+        raise ValueError(f"Tool {name} not found")
 
 if __name__ == "__main__":
     llm = LLMEngine()
-    for chunk in llm.generate_response_stream("Explain Greed"):
-        print(chunk.content)
+    for chunk in llm.generate_response_stream("Play WRONG by Chris Grey."):
+        print(chunk)
